9b20aeb5f5a31eedf1e644cb08b28d784249cde9
Andrea DF94 <defilippiandre@libero.it>
2025-07-08 11:54:07 +0200

    Aggiungo la richiesta input utente per il nome

--- FestaFerragnez.java
+++ FestaFerragnez.java
@@ -9,3 +9,7 @@ public class FestaFerragnez {
             "Chiara Ferragni", "Fedez", "Luis Sal", "Ax", "Francesco Totti",
             "Ilary Blasi", "Bebe Vio", "Lus", "Pardis Zarei", "Martina Maccherone", "Rachel Zelic"
         };
+
+         // Chiedi all'utente il nome
+        System.out.print("Benvenuto alla festa Ferragnez! Come ti chiami? ");
+        String userName = scanner.nextLine();
