a35ed993ca94c58544ca67d67d69f1bfa5aa87eb
Andrea DF94 <defilippiandre@libero.it>
2025-07-08 11:52:18 +0200

    Aggiungo la lista guestlist con gli invitati

--- FestaFerragnez.java
+++ FestaFerragnez.java
@@ -3,3 +3,9 @@ import java.util.Scanner;
 public class FestaFerragnez {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+           // Lista degli invitati
+        String[] guestList = {
+            "Chiara Ferragni", "Fedez", "Luis Sal", "Ax", "Francesco Totti",
+            "Ilary Blasi", "Bebe Vio", "Lus", "Pardis Zarei", "Martina Maccherone", "Rachel Zelic"
+        };
