c6c767815f931db947e701aa6a0dfbcea9c70422
Andrea De Filippi <defilippiandre@libero.it>
2025-07-15 10:13:07 +0200

    creata classe App con metodo main che avvia il programma

---
+++ src/App.java
@@ -0,0 +1,6 @@
+
+public class App { // Classe principale che avvia il programma
+    public static void main(String[] args) { // Metodo main, punto di ingresso del programma
+        FestaFerragnez.main(args); // Richiama il main della classe FestaFerragnez per eseguire la logica
+    }
+}
