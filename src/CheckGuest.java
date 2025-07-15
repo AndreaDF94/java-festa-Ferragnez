061a21a6e76776963d186eac0b51a02e0afdbe39
Andrea De Filippi <defilippiandre@libero.it>
2025-07-15 10:22:20 +0200

    Uso il metodo  containsUsingIf per controllare la presenza sulla lista

--- src/CheckGuest.java
+++ src/CheckGuest.java
@@ -1,1 +1,9 @@
 public class CheckGuest { // Classe che contiene i metodi di verifica degli invitati
+    public static boolean containsUsingIf(String[] list, String name) { // Metodo con ciclo FOR per cercare il nome
+        for (String guest : list) { // Scorre ogni elemento della lista
+            if (guest.equalsIgnoreCase(name)) { // Controlla se il nome corrisponde ignorando maiuscole/minuscole
+                return true; // Se il nome è trovato, restituisce true
+            }
+        }
+        return false; // Se il nome non è nella lista, restituisce false
+    }
