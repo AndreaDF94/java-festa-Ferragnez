a37295fc8bf9542136742cf6f85d27d20cf7734b
Andrea De Filippi <defilippiandre@libero.it>
2025-07-15 10:31:02 +0200

    Creo la classe FestaFerragnez con lista invitati,input utente e utilizzo i metodi di Checkguest

---
+++ src/FestaFerragnez.java
@@ -0,0 +1,6 @@
+import java.util.Scanner; // Importa la classe Scanner per leggere l'input da tastiera
+
+public class FestaFerragnez { // Classe che contiene la logica del programma
+    public static void main(String[] args) { // Metodo main che esegue il programma
+        Scanner scanner = new Scanner(System.in); // Crea un oggetto Scanner per leggere l'input utente
+
